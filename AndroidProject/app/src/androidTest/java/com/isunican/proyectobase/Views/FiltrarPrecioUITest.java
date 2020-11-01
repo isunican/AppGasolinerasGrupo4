c4c787c3b5d0a1962a14d8df6a4370c8e99f98c3
fjavhoz <fjavhoz@gmail.com>
2020-11-01 21:09:57 +0100

    Arreglado un error de programación en la clase de prueba de la interfaz

--- AndroidProject/app/src/androidTest/java/com/isunican/proyectobase/Views/FiltrarPrecioUITest.java
+++ AndroidProject/app/src/androidTest/java/com/isunican/proyectobase/Views/FiltrarPrecioUITest.java
@@ -1,17 +1,19 @@
-package com.isunican.proyectobase;
+package com.isunican.proyectobase.Views;
 
 import androidx.test.espresso.DataInteraction;
 import androidx.test.espresso.ViewInteraction;
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewParent;
+import android.widget.ListView;
 
-import com.isunican.proyectobase.Views.MainActivity;
 import com.isunican.proyectobase.Model.Gasolinera;
-
+import com.isunican.proyectobase.R;
+import com.isunican.proyectobase.Views.MainActivity;
 
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
@@ -23,6 +25,7 @@ import org.junit.runner.RunWith;
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
@@ -41,15 +44,27 @@ public class FiltrarPrecioUITest {
 
         Gasolinera gasolinera;
 
-        //Hace click en la primera gasolinera
-        DataInteraction evento = onData(anything()).inAdapterView(withId(R.id.listViewGasolineras)).atPosition(0);
-        evento.perform(click());
+        //Hace click en la primera gasolinera e introduce un precio mínimo
+        DataInteraction evento1 = onData(anything()).inAdapterView(ViewMatchers.withId(R.id.idPrecioMin));
+        evento1.perform(click());
+        evento1.perform(replaceText("1.8"));
 
-        //Obtiene el objeto Gasolinera de la primera gasolinera de la lista
+        //Hace click en la segunda gasolinera e introduce un precio máximo
+        DataInteraction evento2 = onData(anything()).inAdapterView(ViewMatchers.withId(R.id.idPrecioMax));
+        evento2.perform(click());
+        evento2.perform(replaceText("2"));
+
+        //Obtiene la lista de cada objeto Gasolinera después de aplicar el filtro
         ListView vista = (ListView) mActivityTestRule.getActivity().findViewById(R.id.listViewGasolineras);
-        gasolinera = (Gasolinera) vista.getAdapter().getItem(0);
 
-        onView(withId(R.id.idGasolina)).check(matches(withText(String.valueOf(gasolinera.getGasolina95()))));
-        onView(withId(R.id.idDiesel)).check(matches(withText(String.valueOf(gasolinera.getGasoleoA()))));
+        //Se recorren las gasolineras devueltas y se comprueba que gasolina95 y gasoleoA estan en el rango del filtro
+        for(int i=0;i<vista.getAdapter().getCount();i++){
+            gasolinera = (Gasolinera) vista.getAdapter().getItem(i);
+            //Assert, TODO TENER EN CUENTA EL TIPO DE GASOLINA
+            onView(withId(R.id.idGasolina)).check(matches(withText(String.valueOf(gasolinera.getGasolina95()))));
+            onView(withId(R.id.idDiesel)).check(matches(withText(String.valueOf(gasolinera.getGasoleoA()))));
+        }
+
+
     }
 }
