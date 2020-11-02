b062ddd4af8377fdf757e0d72004797d67f2e2e5
fjavhoz <fjavhoz@gmail.com>
2020-11-02 18:33:46 +0100

    Mejorado clase filtro de combustible

--- AndroidProject/app/src/androidTest/java/com/isunican/proyectobase/Views/FiltrarCombustibleUITest.java
+++ AndroidProject/app/src/androidTest/java/com/isunican/proyectobase/Views/FiltrarCombustibleUITest.java
@@ -44,11 +44,11 @@ public class FiltrarCombustibleUITest {
         Gasolinera gasolinera;
 
         //Hace click en el filtro de Gasoleo A, TODO UTILIZAR BOTON DE FILTRO
-        DataInteraction evento1 = onData(anything()).inAdapterView(ViewMatchers.withId(R.id.idPrecioMin));
+        DataInteraction evento1 = onData(anything()).inAdapterView(ViewMatchers.withId(R.id.idSpinnerCombustible));
         evento1.perform(click());
 
         //Hace click en el filtro de Gasolina 95, TODO LO MISMO QUE ANTES
-        DataInteraction evento2 = onData(anything()).inAdapterView(ViewMatchers.withId(R.id.idPrecioMin));
+        DataInteraction evento2 = onData(anything()).inAdapterView(ViewMatchers.withId(R.id.));
         evento2.perform(click());
 
         //Obtiene la lista de cada objeto Gasolinera después de aplicar el filtro
