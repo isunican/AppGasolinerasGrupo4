996c6c7509587af6ef2d29010b8107f0718b4caa
Victor Perez <perezaberasturi@gmail.com>
2020-11-02 11:20:54 +0100

    Implementacion y modificacion de las pruebas unitarias para la historia de usuario filtrar por tipo de combustible

--- AndroidProject/app/src/test/java/com/isunican/proyectobase/PresenterGasolinerasTest.java
+++ AndroidProject/app/src/test/java/com/isunican/proyectobase/PresenterGasolinerasTest.java
@@ -1,7 +1,7 @@
 package com.isunican.proyectobase;
 
 import com.isunican.proyectobase.Model.Gasolinera;
-import com.isunican.proyectobase.Views.MainActivity;
+import com.isunican.proyectobase.Presenter.PresenterGasolineras;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -9,16 +9,16 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.*;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class MainActivityTest {
+public class PresenterGasolinerasTest {
     // Declaracion de los objetos que vamos a usar en los test
-    private MainActivity sut;
+    private PresenterGasolineras sut;
 
     private List<Gasolinera> gasolinerasCompleto = new ArrayList<Gasolinera>();     //Lista de Gasolineras con gasolineras con distintos combustibles
     private List<Gasolinera> gasolinerasGasoleo = new ArrayList<Gasolinera>();     // Lista de Gasolineras solo con gasoleoA
@@ -33,8 +33,7 @@ public class MainActivityTest {
 
     @Before
     public void setUp() throws Exception{
-        sut = new MainActivity();
-
+        sut = new PresenterGasolineras();
         //Inicializacion de las gasolineras y las listas
         gasoleo = new Gasolinera(0, "","","",1.0, 0.0, "");
         gasoleo2 = new Gasolinera(0, "","","",2.0, 0.0, "");
@@ -58,25 +57,25 @@ public class MainActivityTest {
     public void filtrarCombustibleGasolinaTest()
     {
         // Caso valido UT.1a
-        assertArrayEquals(gasolinerasGasolina, sut.filtrarCombustibleGasolina(gasolinerasCompleto));
+        assertEquals(gasolinerasGasolina, PresenterGasolineras.filtrarCombustibleGasolina(gasolinerasCompleto));
 
         // Caso valido UT.1b
-        assertArrayEquals(gasolinerasVacias, sut.filtrarCombustibleGasolina(gasolinerasGasoleo));
+        assertEquals(gasolinerasVacias, PresenterGasolineras.filtrarCombustibleGasolina(gasolinerasGasoleo));
 
         // Caso valido UT.1c
-        assertArrayEquals(gasolinerasVacias, sut.filtrarCombustibleGasolina(gasolinerasVacias));
+        assertEquals(gasolinerasVacias, PresenterGasolineras.filtrarCombustibleGasolina(gasolinerasVacias));
     }
 
     @Test
     public void filtrarCombustibleGasoleoTest()
     {
         // Caso valido UT.2a
-        assertArrayEquals(gasolinerasGasoleo, sut.filtrarCombustibleGasolina(gasolinerasCompleto));
+        assertEquals(gasolinerasGasoleo, PresenterGasolineras.filtrarCombustibleGasoleo(gasolinerasCompleto));
 
         // Caso valido UT.2b
-        assertArrayEquals(gasolinerasVacias, sut.filtrarCombustibleGasolina(gasolinerasGasolina));
+        assertEquals(gasolinerasVacias, PresenterGasolineras.filtrarCombustibleGasoleo(gasolinerasGasolina));
 
         // Caso valido UT.2c
-        assertArrayEquals(gasolinerasVacias, sut.filtrarCombustibleGasolina(gasolinerasVacias));
+        assertEquals(gasolinerasVacias, PresenterGasolineras.filtrarCombustibleGasoleo(gasolinerasVacias));
     }
 }
