e632997dd582a0360818e7453187455a7601bd68
Victor Perez <perezaberasturi@gmail.com>
2020-11-16 18:51:04 +0100

    Anhadir funcionalidad spinner y mejoras de vehiclesActivity.java y item_vehiculo.xml

--- AndroidProject/app/src/main/java/com/isunican/proyectobase/views/VehiclesActivity.java
+++ AndroidProject/app/src/main/java/com/isunican/proyectobase/views/VehiclesActivity.java
@@ -1,5 +1,4 @@
 package com.isunican.proyectobase.views;
-
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
@@ -26,20 +25,26 @@ import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
-
 
 import com.isunican.proyectobase.R;
 import com.isunican.proyectobase.model.Gasolinera;
 import com.isunican.proyectobase.model.Vehiculo;
+import com.isunican.proyectobase.presenter.PresenterGasolineras;
 import com.isunican.proyectobase.presenter.PresenterVehiculos;
 
 import java.util.List;
 
 public class VehiclesActivity extends AppCompatActivity implements
         AdapterView.OnItemSelectedListener {
+
+    static final String GASOLINA95 = "Gasolina95";
+    static final String GASOLEOA = "GasoleoA";
+
+    //Opciones del spinner
+    String[] combustibles = {GASOLINA95, GASOLEOA};
+
+    String combustibleActual = combustibles[0];
 
     // Vista de lista y adaptador para cargar datos en ella
     ListView listViewVehiculos;
@@ -58,6 +63,7 @@ public class VehiclesActivity extends AppCompatActivity implements
     Spinner spiner_tipo_combustible;
 
 
+
     /**
      * onCreate
      * <p>
@@ -71,49 +77,36 @@ public class VehiclesActivity extends AppCompatActivity implements
         setContentView(R.layout.activity_vehicle);
         presenterVehiculos = new PresenterVehiculos();
         cargaVehiculos(presenterVehiculos.getVehiculos());
-
-        /*
-        // Barra de progreso
-        // https://materialdoc.com/components/progress/
-        progressBar = new ProgressBar(VehiclesActivity.this, null, android.R.attr.progressBarStyleLarge);
-        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(100, 100);
-        params.addRule(RelativeLayout.CENTER_IN_PARENT);
-        RelativeLayout layout = findViewById(R.id.activity_vehicle);
-        layout.addView(progressBar, params);
-
-        // Muestra el logo en el actionBar
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
-        getSupportActionBar().setIcon(R.drawable.por_defecto_mod);
-
-        // Al terminar de inicializar todas las variables
-        // se lanza una tarea para cargar los datos de los vehiculos
-        // Esto se ha de hacer en segundo plano definiendo una tarea asíncrona
-        new CargaDatosVehiculosTask(this).execute();
-        */
     }
 
+
+    /**
+     * Permite seleccionar el combustible del vehiculo que se va a anhadir.
+     *
+     * @param arg0
+     * @param arg1
+     * @param position
+     * @param id
+     */
     @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        //No se espera de momento realizar ninguna accion. Sirve para los spinner
-
+    public void onItemSelected(AdapterView<?> arg0, View arg1, int position, long id) {
+        combustibleActual = combustibles[position];
+        Toast.makeText(getApplicationContext(), combustibles[position], Toast.LENGTH_LONG).show();
     }
-
 
     public void myClickHandler(View view) {
         System.out.println("BOTON");
         btn_anhadirVehiculo = findViewById(R.id.imageButton2);
-        btn_aceptar = findViewById(R.id.idBotonAceptar);
-        btn_cancelar = findViewById(R.id.idBotonCancelar);
 
         btn_anhadirVehiculo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showDialog();
+                anhadirVehiculo();
             }
         });
     }
 
-    private void showDialog(){
+    private void anhadirVehiculo(){
         AlertDialog.Builder alert;
 
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
@@ -122,16 +115,8 @@ public class VehiclesActivity extends AppCompatActivity implements
         else{
             alert = new AlertDialog.Builder(this);
         }
-
         LayoutInflater inflater = getLayoutInflater();
         View view = inflater.inflate(R.layout.activity_alert_dialog, null);
-        txt_Marca = view.findViewById(R.id.idIntroduceMarca);
-        txt_Modelo = view.findViewById(R.id.idIntroduceModelo);
-        txt_Matricula = view.findViewById(R.id.idIntroduceMatricula);
-        btn_aceptar = view.findViewById(R.id.idBotonAceptar);
-        btn_cancelar = view.findViewById(R.id.idBotonCancelar);
-        //Anhadir el spinner
-
         alert.setView(view);
 
         alert.setCancelable(false);
@@ -140,16 +125,40 @@ public class VehiclesActivity extends AppCompatActivity implements
         dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
         dialog.show();
 
+        //Cogemos el spinner
+        spiner_tipo_combustible = (Spinner) view.findViewById(R.id.idSpinnerCombustible);
+        spiner_tipo_combustible.setOnItemSelectedListener(this);
+
+        //Creamos el arrayAdapter con la lista del spinner
+        ArrayAdapter<String> aa = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, combustibles);
+        aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        //Colocamos los datos en el spinner
+        spiner_tipo_combustible.setAdapter(aa);
+
+        txt_Marca = view.findViewById(R.id.idIntroduceMarca);
+        txt_Modelo = view.findViewById(R.id.idIntroduceModelo);
+        txt_Matricula = view.findViewById(R.id.idIntroduceMatricula);
+        btn_aceptar = view.findViewById(R.id.idBotonAceptar);
+        btn_cancelar = view.findViewById(R.id.idBotonCancelar);
+
         btn_aceptar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String mar = txt_Marca.getText().toString();
                 String model = txt_Modelo.getText().toString();
                 String matric = txt_Matricula.getText().toString();
-                Vehiculo vehiculo = new Vehiculo(1 ,mar, model, matric, "gasolina");
+
+                Vehiculo vehiculo = new Vehiculo(1 ,mar, model, matric, combustibleActual);
+                //vehiculo.escribeVehiculo();
+                //Leer json para mostrar lista
+
                 presenterVehiculos.getVehiculos().add(vehiculo);
+
+                //Llamada al metodo de escritura en JSON
                 Toast.makeText(getApplicationContext(), "Datos añadidos", Toast.LENGTH_LONG).show();
                 dialog.dismiss();
+                //Llamada para escribir en el json
                 cargaVehiculos(presenterVehiculos.getVehiculos());
             }
         });
@@ -161,8 +170,6 @@ public class VehiclesActivity extends AppCompatActivity implements
                 dialog.dismiss();
             }
         });
-
-
     }
 
     private void cargaVehiculos(List<Vehiculo> vehiculos) {
@@ -194,105 +201,6 @@ public class VehiclesActivity extends AppCompatActivity implements
     public void onNothingSelected(AdapterView<?> parent) {
         //De momento no hace nada
     }
-
-
-    /**
-     * CargaDatosVehiculosTask
-     * <p>
-     * Tarea asincrona para obtener los datos de los vehiculos
-     * en segundo plano.
-     * <p>
-     * Redefinimos varios métodos que se ejecutan en el siguiente orden:
-     * onPreExecute: activamos el dialogo de progreso
-     * doInBackground: solicitamos que el presenter cargue los datos
-     * onPostExecute: desactiva el dialogo de progreso,
-     * muestra los vehiculos en formato lista (a partir de un adapter)
-     * y define la acción al realizar al seleccionar alguna de ellas
-     * <p>
-     * http://www.sgoliver.net/blog/tareas-en-segundo-plano-en-android-i-thread-y-asynctask/
-     */
-    private class CargaDatosVehiculosTask extends AsyncTask<Void, Void, Boolean> {
-        Activity activity;
-
-        /**
-         * Constructor de la tarea asincrona
-         *
-         * @param activity
-         */
-        public CargaDatosVehiculosTask(Activity activity) {
-            this.activity = activity;
-        }
-
-        /**
-         * onPreExecute
-         * <p>
-         * Metodo ejecutado de forma previa a la ejecucion de la tarea definida en el metodo doInBackground()
-         * Muestra un diálogo de progreso
-         * @deprecated
-         */
-        @Override
-        @Deprecated
-        protected void onPreExecute() {
-            progressBar.setVisibility(View.VISIBLE);  //To show ProgressBar
-        }
-
-        /**
-         * doInBackground
-         * <p>
-         * Tarea ejecutada en segundo plano
-         * Llama al presenter para que lance el método de carga de los datos de los vehiculos
-         * @deprecated
-         * @param params
-         * @return boolean
-         */
-        @Override
-        @Deprecated
-        protected Boolean doInBackground(Void... params) {
-            return presenterVehiculos.cargaDatosVehiculos();
-        }
-
-        /**
-         * onPostExecute
-         * <p>
-         * Se ejecuta al finalizar doInBackground
-         * Oculta el diálogo de progreso.
-         * Muestra en una lista los datos de las vehiculos cargados,
-         * creando un adapter y pasándoselo a la lista.
-         * Al pulsar al boton de añadir vehiculo lanzará un alert dialog
-         * para introducir los datos de un nuevo vehiculo
-         * @deprecated
-         *
-         * @param res
-         */
-        @Override
-        @Deprecated
-        protected void onPostExecute(Boolean res) {
-            boolean result = res;
-
-            // Si el progressDialog estaba activado, lo oculta
-            progressBar.setVisibility(View.GONE);     // To Hide ProgressBar
-
-            /*
-             * Define el manejo de los eventos de click sobre elementos de la lista
-             * En este caso, al pulsar un elemento se lanzará una actividad con una vista de detalle
-             * a la que le pasamos el objeto Gasolinera sobre el que se pulsó, para que en el
-             * destino tenga todos los datos que necesita para mostrar.
-             * Para poder pasar un objeto Gasolinera mediante una intent con putExtra / getExtra,
-             * hemos tenido que hacer que el objeto Gasolinera implemente la interfaz Parcelable
-             */
-            listViewVehiculos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-                public void onItemClick(AdapterView<?> a, View v, int position, long id) {
-                    /* Obtengo el elemento directamente de su posicion,
-                     * ya que es la misma que ocupa en la lista
-                     */
-                    Intent myIntent = new Intent(VehiclesActivity.this, DetailActivity.class);
-                    myIntent.putExtra(getResources().getString(R.string.pasoDatosVehiculo), presenterVehiculos.getVehiculos().get(position));
-                    VehiclesActivity.this.startActivity(myIntent);
-                }
-            });
-        }
-    }
-
 
     /*
     ------------------------------------------------------------------
@@ -365,7 +273,6 @@ public class VehiclesActivity extends AppCompatActivity implements
 
         private void cargaIcono(Vehiculo vehiculo, ImageView logo) {
             String rotuleImageID = vehiculo.getMarca().toLowerCase();
-
             // Tengo que protegerme ante el caso en el que el rotulo solo tiene digitos.
             // En ese caso getIdentifier devuelve esos digitos en vez de 0.
             int imageID = context.getResources().getIdentifier(rotuleImageID,
